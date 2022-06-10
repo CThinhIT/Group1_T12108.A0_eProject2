e1ce5a44a255307ac75fdf59c68743ed03e15413
LAPTOP-FAK4QUSA\NGOC THAI <hnthai1710@gmail.com>
2022-06-10 16:27:56 +0700

    no message

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,58 @@
+<!-- Footer Start -->
+<div class="footer">
+    <!-- Footer -->
+    <footer class="bg-dark text-white">
+        <!-- Grid container -->
+        <div class="row">
+            <div class="col-2">
+                <div class="container text-start">
+                    <img src={{asset('eshoper/img/sidebar/logo_gamegear1.png')}} alt="">
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="container p-4 text-center" style="padding-top: 10px">
+                    <!-- Section: Social media -->
+                    <section class="mb-4">
+                        <!-- Facebook -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                        <!-- Twitter -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                        <!-- Google -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="mailto:congthinh.0311@gmail.com" role="button"><i class="fab fa-google"></i></a>
+
+                        <!-- Instagram -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                        <!-- Github -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+                    </section>
+                    <!-- Section: Social media -->
+
+                    <!-- Section: Form -->
+
+                    <!-- Section: Form -->
+
+                    <!-- Section: Text -->
+                    <section class="mb-4">
+                        <p>Hotline: 210.820.03</p>
+                        <p>Address: 590 CMT8, Ward 11, District 3, Ho Chi Minh City.</p>
+                    </section>
+                </div>
+                <!-- Grid container -->
+
+                <!-- Copyright -->
+
+            </div>
+            <div class="col-2"></div>
+
+
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2022 Copyright: Developed by group 1, class T1.2108.A0
+        </div>
+        <!-- Copyright -->
+    </footer>
+</div>
+<!-- Footer End -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/test', [HomeController::class, 'test'])->name('test');
+
+   
